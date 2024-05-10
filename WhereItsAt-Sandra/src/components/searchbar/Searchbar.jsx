21b13999a858d393bccb3649cb0189406d9f80ca
sandranymark <sandra@nymark.nu>
2024-05-10 15:17:54 +0200

    Icon in searchbar

--- WhereItsAt-Sandra/src/components/searchbar/Searchbar.jsx
+++ WhereItsAt-Sandra/src/components/searchbar/Searchbar.jsx
@@ -1,22 +1,16 @@
-import SearchIcon from '../../assets/icons/search-icon.svg'
+
 import './Searchbar.css'
-
-
+import { FaSearch } from 'react-icons/fa';
 
 function Searchbar() {
     return (
-        <>
-            <div className='search__container'>
-                <input type="search" className='search__input'
-                    aria-label="Serchbar" />
-                <img
-                    className='search__icon'
-                    src={SearchIcon}
-                    alt='search icon' />
-
+        <div className='search__container'>
+            <input type="search" className='search__input' aria-label="Searchbar" />
+            <div className='search__icon'>
+                <FaSearch />
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
-export default Searchbar
+export default Searchbar;
